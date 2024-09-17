9c3e1bbe0cd04fbdb2d4310ebd2178c2a651e383
LaLunaKernel <lalunakernel@gmail.com>
2024-09-17 16:37:12 +0200

    Create offsets.hpp

---
+++ offsets.hpp
@@ -0,0 +1,14 @@
+namespace Offsets {
+    uintptr_t UWorld = 0x1215C178;
+    uintptr_t GObjects = 0x12133D70;
+    uintptr_t GNames = 0x1233C1A0;
+
+    uintptr_t Mesh = 0x310; // ACharacter->Mesh
+    uintptr_t BoneArray = 0x570;
+    uintptr_t LastSubmitTime = 0x2E0;
+    uintptr_t LastRenderTime = 0x2E8;
+    uintptr_t TeamIndex = 0x1211;
+    uintptr_t ProjectileSpeed = 0x1E40;
+    uintptr_t ProjectileGravity = 0x1E44;
+    uintptr_t PlayerName = 0xAA8;
+}
