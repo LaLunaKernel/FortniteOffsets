7a919d29faba3f4d0211f2f49df115c3343eb9a3
LaLunaKernel <lalunakernel@gmail.com>
2025-01-20 20:05:07 +0100

    Update GetBoneWithRotation.h

--- GetBoneWithRotation.h
+++ GetBoneWithRotation.h
@@ -2,7 +2,7 @@ static auto getsocketlocation(uintptr_t skeletal_mesh, int bone_index) -> fvecto
 		uintptr_t bone_array = read<uintptr_t>(skeletal_mesh + offsets::BoneArray);
 		if (bone_array == NULL) bone_array = read<uintptr_t>(skeletal_mesh + offsets::BoneArray + 0x10);
 
-		FTransform bone = io->read<FTransform>(bone_array + (bone_index * 0x60));
+		FTransform bone = read<FTransform>(bone_array + (bone_index * 0x60));
 		FTransform component_to_world = read<FTransform>(skeletal_mesh + offsets::ComponentToWorld);
 		D3DMATRIX matrix = matrix_multiplication(bone.to_matrix_with_scale(), component_to_world.to_matrix_with_scale());
 
